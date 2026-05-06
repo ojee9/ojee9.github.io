6f10600ca6f45281468d5bbaef5cca6e74051398
ojee9 <165341079+ojee9@users.noreply.github.com>
2026-05-07 01:57:08 +0300

    Update hot.js

--- config/hot.js
+++ config/hot.js
@@ -1,1 +1,10 @@
+window.SCENES = window.SCENES || {};
 
+window.SCENES.HOT = {
+  bg: "images/desert.jpg",
+  skin: "skins/papyrus.css",
+  highlights: [
+    { title:"Hot Track 1", url:"https://soundcloud.com/9ojeez9" },
+    { title:"Hot Track 2", url:"https://soundcloud.com/9ojeez9" }
+  ]
+};
